8e42d04bc73468380ee7f2a9a0213537020967e4
Songhareem <ignisilva92@gmail.com>
2021-11-23 14:47:19 +0900

    update fizzbuzz.js
    else case, print now number or print 'fizzbuzz'

---
+++ fizzbuzz.js
@@ -0,0 +1,19 @@
+function fizzbuzz(n) {
+  for (let i = 1; i <= n; i++) {
+    let fizzbuzz = "";
+
+    // 3의 배수일땐 'fizz'
+    // 5의 배수일땐 'buzz'
+    // 15의 배수일땐 'fizzbuzz'
+    // 나머지 경우엔 숫자
+    if (fizzbuzz === "") {
+      console.log(i);
+    } else {
+      console.log(fizzbuzz);
+    }
+  }
+}
+
+const n = 31;
+
+fizzbuzz(n);
