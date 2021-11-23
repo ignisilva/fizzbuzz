09d8f559ca96284497b3ab752556347a14d79150
Songhareem <ignisilva92@gmail.com>
2021-11-23 14:53:01 +0900

    update fizzbuzz.js
    multiple of 5, print 'buzz'
    
    multiple of 15, print 'fizzbuzz'

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -2,10 +2,12 @@ function fizzbuzz(n) {
   for (let i = 1; i <= n; i++) {
     let fizzbuzz = "";
 
-    if (i % 3 === 0) fizzbuzz += "fizz";
     // 3의 배수일땐 'fizz'
+    if (i % 3 === 0) fizzbuzz += "fizz";
+
     // 5의 배수일땐 'buzz'
-    // 15의 배수일땐 'fizzbuzz'
+    if (i % 5 === 0) fizzbuzz += "buzz";
+
     // 나머지 경우엔 숫자
     if (fizzbuzz === "") {
       console.log(i);
