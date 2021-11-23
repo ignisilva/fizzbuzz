9bb9b1c47861903e0b6cfc018315a694520af440
Songhareem <ignisilva92@gmail.com>
2021-11-23 15:01:31 +0900

    update fizzbuzz.js
    When n is list, execution fizzbuzz algorithm about each element of list

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,22 +1,25 @@
 function fizzbuzz(n) {
-  for (let i = 1; i <= n; i++) {
-    let fizzbuzz = "";
+  n.forEach((num) => {
+    for (let i = 1; i <= num; i++) {
+      let fizzbuzz = "";
 
-    // 3의 배수일땐 'fizz'
-    if (i % 3 === 0) fizzbuzz += "fizz";
+      // 3의 배수일땐 'fizz'
+      if (i % 3 === 0) fizzbuzz += "fizz";
 
-    // 5의 배수일땐 'buzz'
-    if (i % 5 === 0) fizzbuzz += "buzz";
+      // 5의 배수일땐 'buzz'
+      if (i % 5 === 0) fizzbuzz += "buzz";
 
-    // 나머지 경우엔 숫자
-    if (fizzbuzz === "") {
-      console.log(i);
-    } else {
-      console.log(fizzbuzz);
+      // 나머지 경우엔 숫자
+      if (fizzbuzz === "") {
+        console.log(i);
+      } else {
+        console.log(fizzbuzz);
+      }
     }
-  }
+    console.log();
+  });
 }
 
-const n = 100;
+const n = [31, 100];
 
 fizzbuzz(n);
